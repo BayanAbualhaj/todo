5509468faa89fb3ec777f5232e902b2b000d892e
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-05-20 01:00:29 +0300

    lab34

--- src/app.js
+++ src/app.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { If, Else, Then } from 'react-if';
 import NavBar from "./components/todo/navBar";
 import ToDo from './components/todo/todo.js';
 import PaginationProvider from './components/context/pagination';
+import Auth from './components/auth/auth.js';
+import { LoginContext } from './components/auth/context.js';
+
+
+const EditLink = () => {
+	return (
+		<Auth capability="update">
+			<span>Edit</span>
+		</Auth>
+	);
+};
+
+const DeleteLink = () => {
+	return (
+		<Auth capability="delete">
+			<span>Delete</span>
+		</Auth>
+	);
+};
+
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <PaginationProvider>
-        <ToDo />
-      </PaginationProvider>
-    </>
-  );
+	const loginContext = useContext(LoginContext);
+
+	return (
+		<>
+			<NavBar />
+			<EditLink />
+			<DeleteLink />
+			<If condition={loginContext.loggedIn}>
+				<Then>
+					<PaginationProvider>
+						<ToDo />
+					</PaginationProvider>
+				</Then>
+				<Else>
+					<div></div>
+				</Else>
+			</If>
+		</>
+	);
 }
 
 export default App;
--- src/components/todo/navBar.js
+++ src/components/todo/navBar.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Login from './../auth/login';
+import SignUp from './../auth/signUp';
 
 
 
@@ -11,6 +13,8 @@ const NavBar = (props) => {
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
             </Nav>
+            <Login />
+			<SignUp />
         </Navbar>
     )
 }
